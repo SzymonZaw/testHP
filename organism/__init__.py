4e5045df9295390125a41a50e22d4db743c54370
SzymonZawadzki <88500628+SzymonZaw@users.noreply.github.com>
2026-08-13 14:07:13 +0200

    Export integrated health state

--- organism/__init__.py
+++ organism/__init__.py
@@ -1,5 +1,11 @@
 """Whole-body biological state model."""
 
 from .organism_model import OrganismState, OrganismModel
+from .health_state import HealthState, HealthStateAggregator
 
-__all__ = ["OrganismState", "OrganismModel"]
+__all__ = [
+    "OrganismState",
+    "OrganismModel",
+    "HealthState",
+    "HealthStateAggregator",
+]
