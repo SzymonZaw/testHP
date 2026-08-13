dfebbb0d16ae113393b30ed9bd0bb11ff3f0c42e
SzymonZawadzki <88500628+SzymonZaw@users.noreply.github.com>
2026-08-13 14:08:32 +0200

    Export longitudinal trajectory API

---
+++ longitudinal/__init__.py
@@ -0,0 +1,5 @@
+"""Longitudinal biological trajectory analysis."""
+
+from .trajectory import TrajectoryAnalyzer, TrajectoryPoint, Trend
+
+__all__ = ["TrajectoryAnalyzer", "TrajectoryPoint", "Trend"]
