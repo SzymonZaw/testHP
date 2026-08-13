a7427c3ac220f892aeb712f60a3b09eee690ac0d
SzymonZawadzki <88500628+SzymonZaw@users.noreply.github.com>
2026-08-13 13:53:35 +0200

    Add organ modeling package

---
+++ organs/__init__.py
@@ -0,0 +1,5 @@
+"""Organ-level biological state modeling primitives."""
+
+from .organ_model import OrganModel, OrganState, OrganSystemModel
+
+__all__ = ["OrganModel", "OrganState", "OrganSystemModel"]
