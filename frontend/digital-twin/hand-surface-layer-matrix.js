b6962a7b6801d42bd8232a225b0309ee8362209c
SzymonZawadzki <88500628+SzymonZaw@users.noreply.github.com>
2026-08-27 13:10:47 +0200

    Add layer matrix contract

---
+++ frontend/digital-twin/hand-surface-layer-matrix.js
@@ -0,0 +1,9 @@
+(() => {
+  'use strict';
+  window.testhpLayerMatrix = {
+    macro: ['photo','measurement','observation'],
+    tissue: ['photo','histology','observation'],
+    cellular: ['microscopy','marker','observation'],
+    cell: ['molecular','expression','observation']
+  };
+})();
