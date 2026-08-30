54015dac2ba7476ed40a39698872142ff53bcd6c
SzymonZawadzki <88500628+SzymonZaw@users.noreply.github.com>
2026-08-30 13:45:52 +0200

    feat(spatial): expose spatial adapter public API

---
+++ frontend/digital-twin/spatial/index.js
@@ -0,0 +1,7 @@
+export * from "./spatial-types.js";
+export * from "./spatial-validator.js";
+export * from "./spatial-evidence.js";
+export * from "./spatial-data-adapter.js";
+export * from "./spatial-importer.js";
+export * from "./spatial-three.js";
+export * from "./spatial-state.js";
